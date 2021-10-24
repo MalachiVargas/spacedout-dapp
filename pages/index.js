896bec8f9e2815ee9eaf4aa6ac53c47884462368
root <root@DESKTOP-T0URK6P.localdomain>
2021-10-24 10:02:49 -0400

    landing page start

--- pages/index.js
+++ pages/index.js
@@ -1,67 +1,171 @@
 import Head from 'next/head'
-import Navbar from '../components/Navbar'
+import { Fragment } from 'react'
+import { Popover, Transition } from '@headlessui/react'
+import { MenuIcon, XIcon } from '@heroicons/react/outline'
+
+const navigation = [
+
+]
 
 export default function Home() {
   return (
-    <div className="">
-      <Navbar />
+    <div className="relative bg-white overflow-hidden">
       <Head>
         <title>SpacedOut</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className="max-w-7xl mx-auto">
+        <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl  lg:pb-28 xl:pb-32">
+          <svg
+            className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
+            fill="currentColor"
+            viewBox="0 0 100 100"
+            preserveAspectRatio="none"
+            aria-hidden="true"
+          >
+            <polygon points="50,0 100,0 50,100 0,100" />
+          </svg>
 
-      <main className="">
-        <section className="">
-          <div className="">
-            <div className="">
-              <div className="">
-                <h2 className="">
-                  Notus React - A beautiful extension for Tailwind CSS.
-                </h2>
-                <p className="mt-4 text-lg leading-relaxed text-blueGray-500">
-                  Notus React is Free and Open Source. It does not change any of
-                  the CSS from{" "}
+          <Popover>
+            <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
+              <nav className="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
+                <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+                  <div className="flex items-center justify-between w-full md:w-auto">
+                    <a href="#">
+                      <span className="sr-only">SpacedOut</span>
+                      <img
+                        className="h-20 w-auto sm:h-20"
+                        src="spacedout-logo.svg"
+                      />
+                    </a>
+                    <div className="-mr-2 flex items-center md:hidden">
+                      <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                        <span className="sr-only">Open main menu</span>
+                        <MenuIcon className="h-6 w-6" aria-hidden="true" />
+                      </Popover.Button>
+                    </div>
+                  </div>
+                </div>
+                <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
+                  {navigation.map((item) => (
+                    <a key={item.name} href={item.href} className="font-medium text-gray-500 hover:text-gray-900">
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+              </nav>
+            </div>
+
+            <Transition
+              as={Fragment}
+              enter="duration-150 ease-out"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="duration-100 ease-in"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Popover.Panel
+                focus
+                className="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+              >
+                <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
+                  <div className="px-5 pt-4 flex items-center justify-between">
+                    <div>
+                      <img
+                        className="h-8 w-auto"
+                        src="spacedout-logo.svg"
+                        alt="logo"
+                      />
+                    </div>
+                    <div className="-mr-2">
+                      <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                        <span className="sr-only">Close main menu</span>
+                        <XIcon className="h-6 w-6" aria-hidden="true" />
+                      </Popover.Button>
+                    </div>
+                  </div>
+                  <div className="px-2 pt-2 pb-3 space-y-1">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </Popover.Panel>
+            </Transition>
+          </Popover>
+
+          <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+            <div className="sm:text-center lg:text-left">
+              <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                <span className="block xl:inline">Data to enrich your</span>{' '}
+                <span className="block text-indigo-600 xl:inline">online business</span>
+              </h1>
+              <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
+                fugiat veniam occaecat fugiat aliqua.
+              </p>
+              <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+                <div className="rounded-md shadow">
                   <a
-                    href="https://tailwindcss.com/?ref=creativetim"
-                    className=""
-                    target="_blank"
-                  >
-                    Tailwind CSS
-                  </a>
-                  . It features multiple HTML elements and it comes with dynamic
-                  components for ReactJS, Vue and Angular.
-                </p>
-                <div className="mt-12">
-                  <a
-                    href="https://www.creative-tim.com/learning-lab/tailwind/react/overview/notus?ref=nr-index"
-                    target="_blank"
-                    className=""
+                    href="#"
+                    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
                   >
                     Get started
                   </a>
+                </div>
+                <div className="mt-3 sm:mt-0 sm:ml-3">
                   <a
-                    href="https://github.com/creativetimofficial/notus-react?ref=nr-index"
-                    className=""
-                    target="_blank"
+                    href="#"
+                    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
                   >
-                    Github Star
+                    Live demo
                   </a>
                 </div>
               </div>
             </div>
+          </main>
+        </div>
+      </div>
+      <div className="lg:absolute lg:inset-y-14 lg:right-0 lg:w-1/2">
+        <img
+          className="w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-screen"
+          src="so-example.png"
+          alt=""
+        />
+        
+      </div>
+
+      <footer className="text-gray-600 body-font relative bg-white">
+        <div className="bg-gray-100">
+          <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+            <p className="text-gray-500 text-sm text-center sm:text-left">© 2021 SpacedOut NFT —
+              <a href="https://twitter.com/spacedoutnft" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">@spacedoutnft</a>
+            </p>
+            <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+              <a className="mr-3 text-gray-500">
+                <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                  <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                </svg>
+              </a>
+              <a cclassName="ml-3 text-gray-500">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                  <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                  <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                </svg>
+              </a>
+            </span>
           </div>
-
-          <img
-            className=""
-            src="so-example.png"
-            alt="..."
-          />
-        </section>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <img src="/spacedout-logo.svg" alt="SpacedOut Logo" className="h-20 ml-2" />
+        </div>
       </footer>
     </div>
   )
 }
+
+
