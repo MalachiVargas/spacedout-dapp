b90c46ec403e608e84acebb613ca8284894b6198
Malachi Vargas <mcvarga7@gmail.com>
2021-10-26 01:21:27 -0400

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,81 +1,66 @@
 import Head from 'next/head'
+import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="">
+      <Navbar />
       <Head>
-        <title>Create Next App</title>
+        <title>SpacedOut</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      <main className="">
+        <section className="">
+          <div className="">
+            <div className="">
+              <div className="">
+                <h2 className="">
+                  Notus React - A beautiful extension for Tailwind CSS.
+                </h2>
+                <p className="mt-4 text-lg leading-relaxed text-blueGray-500">
+                  Notus React is Free and Open Source. It does not change any of
+                  the CSS from{" "}
+                  <a
+                    href="https://tailwindcss.com/?ref=creativetim"
+                    className=""
+                    target="_blank"
+                  >
+                    Tailwind CSS
+                  </a>
+                  . It features multiple HTML elements and it comes with dynamic
+                  components for ReactJS, Vue and Angular.
+                </p>
+                <div className="mt-12">
+                  <a
+                    href="https://www.creative-tim.com/learning-lab/tailwind/react/overview/notus?ref=nr-index"
+                    target="_blank"
+                    className=""
+                  >
+                    Get started
+                  </a>
+                  <a
+                    href="https://github.com/creativetimofficial/notus-react?ref=nr-index"
+                    className=""
+                    target="_blank"
+                  >
+                    Github Star
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+          <img
+            className=""
+            src="so-example.png"
+            alt="..."
+          />
+        </section>
       </main>
 
       <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
+        <img src="/spacedout-logo.svg" alt="SpacedOut Logo" className="h-20 ml-2" />
       </footer>
     </div>
   )
